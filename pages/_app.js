9feba6d2e8502780f37b1aabfb4f1907e69da961
Jordan <wirl.jordan@gmail.com>
2025-08-07 21:43:34 -0400

    🚀 Force Vercel rebuild by updating _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
 
+// Force Vercel deployment - test buttons must be visible
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
